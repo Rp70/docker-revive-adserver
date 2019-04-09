2a5544ccfbda6246a97a9d554b39111ad4e02370
Rp70 <14834373+Rp70@users.noreply.github.com>
2019-04-09 14:26:54 +0700

    commit

--- files/docker-start-hook.sh
+++ files/docker-start-hook.sh
@@ -2,7 +2,7 @@
 set -e
 
 
-sed -i "s/\bserver_name adserver\;/server_name $SERVER_NAME;/g" /etc/nginx/conf.d/default.conf
+sed -i "s/\bserver_name revive-adserver\;/server_name $SERVER_NAME;/g" /etc/nginx/conf.d/default.conf
 
 # Disable backups
 touch /var/www/html/var/NOBACKUPS
