108c8c824a984115bb06ab9beac0eb4160b14acc
Rp70 <14834373+Rp70@users.noreply.github.com>
2019-04-09 22:19:25 +0700

    commit

--- files/docker-start-hook.sh
+++ files/docker-start-hook.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 set -e
 
-
+# Setting some customs
 sed -i "s/\bserver_name revive-adserver\;/server_name $SERVER_NAME;/g" /etc/nginx/sites-available/revive-adserver.conf
 ln -s /etc/nginx/sites-available/revive-adserver.conf /etc/nginx/sites-enabled/
 
 # Disable backups
 touch /var/www/html/var/NOBACKUPS
+
+# Setting permissions
+find /var/www/html/var -type d -exec chmod 700 {} + && \
+find /var/www/html/var -type f -exec chmod 600 {} + && \
+chmod 700 /var/www/html/plugins && \
+chmod 700 /var/www/html/www/admin/plugins && \
+chown -R www-data:www-data /var/www/html
