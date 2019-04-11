c96c09abd5802f59b9f4b4f8d850e21b30416400
Rp70 <14834373+Rp70@users.noreply.github.com>
2019-04-11 11:08:32 +0700

    commit

--- files/entrypoint-hook-end.sh
+++ files/entrypoint-hook-end.sh
@@ -5,10 +5,15 @@ set -e
 sed -i "s/\bserver_name revive-adserver\;/server_name $SERVER_NAME;/g" /etc/nginx/sites-available/revive-adserver.conf
 ln -s /etc/nginx/sites-available/revive-adserver.conf /etc/nginx/sites-enabled/
 
-# Disable backups
-touch /var/www/html/var/NOBACKUPS
-#touch /var/www/html/var/INSTALLED
+# Disable backups if required
+if [ "$REVIVE_NOBACKUPS" = '1' ]; then
+    touch /var/www/html/var/NOBACKUPS
+fi
 
+# Setting maintenance script run by crond
+if [ "$REVIVE_MAINTENANCE" = 'cron' ]; then
+    echo -e "#!/bin/sh\nphp /var/www/html/maintenance/maintenance.php $SERVER_NAME" > /etc/cron.hourly/revive-adserver
+fi
 
 # Setting permissions
 find /var/www/html/var -type d -exec chmod 700 {} + && \
@@ -16,3 +21,15 @@ find /var/www/html/var -type f -exec chmod 600 {} + && \
 chmod 700 /var/www/html/plugins && \
 chmod 700 /var/www/html/www/admin/plugins && \
 chown -R www-data:www-data /var/www/html
+
+# Clean up current cache if any
+rm -rdf /var/www/html/var/templates_compiled/** /var/www/html/var/cache/**;
+
+if [ "$REVIVE_SECURE" = '1' ]; then
+    if [ -e /var/www/html/var/INSTALLED ]; then
+        rm -rf /var/www/html/www/admin/install.php \
+                /var/www/html/www/admin/install-plugin.php \
+                /var/www/html/www/admin/install-runtask.php;
+        chmod -c 0444 /var/www/html/var/$SERVER_NAME.conf.php;
+    fi
+fi
